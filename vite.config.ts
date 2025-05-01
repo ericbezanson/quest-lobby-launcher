04209bceb391efc9d20d14fdadc3fc52e75e8b59
Tyler <tyler.rudolph@redspace.com>
2025-05-01 11:56:31 -0300

    update vite base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./docs",  // Set the base URL for GitHub Pages
+  base: "/quest-lobby-launcher/",  // Set the base URL for GitHub Pages
   server: {
     host: "::",
     port: 8080,
